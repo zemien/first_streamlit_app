c91bb29e5f8bb310faabc74afcf1d5c2c03251d7
Shawn <zemien@users.noreply.github.com>
2023-07-30 23:10:23 +1200

    Update streamlit_app.py
    support fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,11 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # normalize the json response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
