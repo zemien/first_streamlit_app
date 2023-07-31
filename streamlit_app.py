6ccee9c22757403dd129c86511383e0e734346ec
Shawn <zemien@users.noreply.github.com>
2023-07-31 21:45:14 +1200

    Update streamlit_app.py
    control of flow issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,5 @@ streamlit.dataframe(my_data_rows)
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('test from streamlit')")
